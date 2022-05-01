ec7fe88f0eca7e9b81ec846a6b52cf9f611e8627
mj <aaa5683@exntu.com>
2022-05-02 03:20:20 +0900

    correct typo

--- transcode_u30_1.sh
+++ transcode_u30_1.sh
@@ -18,7 +18,7 @@ FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} \
 out_1_width=1280: out_1_height=720:  out_1_rate=full: \
 out_2_width=1920: out_2_height=1080: out_2_rate=half: \
 out_3_width=2560: out_3_height=1440: out_3_rate=half: \
-out_4_width=3840: out_4_height=2160: out_3_rate=full [a][b][c][d]' \
+out_4_width=3840: out_4_height=2160: out_4_rate=half [a][b][c][d]' \
 -map '[a]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p30.mp4 \
 -map '[b]' -cores 4 -b:v 4M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1080p15.mp4 \
 -map '[c]' -cores 4 -b:v 6M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1440p15.mp4 \
