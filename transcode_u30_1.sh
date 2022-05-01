f8c54a827534d53f2b28e8b1794282b97d8294d3
mj <aaa5683@exntu.com>
2022-05-02 03:36:35 +0900

    decrease output 5

--- transcode_u30_1.sh
+++ transcode_u30_1.sh
@@ -18,15 +18,13 @@ FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} \
 out_1_width=1280: out_1_height=720:  out_1_rate=full: \
 out_2_width=1280: out_2_height=720:  out_2_rate=half: \
 out_3_width=1920: out_3_height=1080: out_3_rate=full: \
-out_4_width=1920: out_4_height=1080: out_4_rate=half: \
-out_5_width=2560: out_5_height=1440: out_5_rate=half: \
-out_6_width=3840: out_6_height=2160: out_6_rate=half [a][b][c][d][e][f]' \
+out_4_width=2560: out_4_height=1440: out_4_rate=half: \
+out_5_width=3840: out_5_height=2160: out_5_rate=half [a][b][c][d][e]' \
 -map '[a]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p30.mp4 \
 -map '[b]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p15.mp4 \
 -map '[c]' -cores 4 -b:v 4M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1080p30.mp4 \
--map '[d]' -cores 4 -b:v 4M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1080p15.mp4 \
--map '[e]' -cores 4 -b:v 6M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1440p15.mp4 \
--map '[f]' -cores 4 -b:v 8M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_4k30.mp4"
+-map '[d]' -cores 4 -b:v 6M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1440p15.mp4 \
+-map '[e]' -cores 4 -b:v 8M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_4k30.mp4"
 
 cmd="time ffmpeg -hide_banner ${FFMPEG_ARGS}"
 
