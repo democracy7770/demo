c15e3edca17f26d4f9609fbd7691c4452ebfda38
mj <aaa5683@exntu.com>
2022-05-03 00:08:34 +0900

    transcode_1.sh testing

--- transcode_1.sh
+++ transcode_1.sh
@@ -4,9 +4,9 @@ INPUT_FILE=$1
 OUTPUT_DIR=$2
 OUTPUT_FILE_PREFIX_NAME=$3
 
-#FFMPEG_ARGS="-i ${INPUT_FILE} -vf 'scale=3840x2160:flags=lanczos' -c:v libx264 -c:a copy -r 60 -b:v 1M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k.mp4"
+FFMPEG_ARGS="-i ${INPUT_FILE} -vf 'scale=3840x2160:flags=lanczos' -c:v libx264 -threads 4 -c:a aac -r 15 -b:v 8M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k15fps_1.mp4"
 
-FFMPEG_ARGS="-i ${INPUT_FILE} -s 3840x2160 -c:v libx264 -threads 4 -c:a aac -b:v 8M -r 15 -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k15fps.mp4"
+#FFMPEG_ARGS="-i ${INPUT_FILE} -s 3840x2160 -c:v libx264 -threads 4 -c:a aac -b:v 8M -r 15 -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k15fps.mp4"
 
 cmd="time ffmpeg_nou30 -hide_banner ${FFMPEG_ARGS}"
 
