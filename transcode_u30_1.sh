6b750f95deee6a334a57aad40333c8ef82c6b6ca
mj <aaa5683@exntu.com>
2022-05-02 23:14:54 +0900

    threads option testing

--- transcode_u30_1.sh
+++ transcode_u30_1.sh
@@ -13,20 +13,20 @@ source /opt/xilinx/xcdr/setup.sh
 #FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} -filter_complex 'multiscale_xma=outputs=1: out_1_width=3840: out_1_height=2160: out_1_rate=full [a]; asplit=outputs=1 aud' -map 'a' -cores 4 -c:v mpsoc_vcu_h264 -map 'aud' -c:a aac -f mp4 -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k_u30.mp4"
 #FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} -filter_complex 'multiscale_xma=outputs=1: out_1_width=3840: out_1_height=2160: out_1_rate=full [a]' -map '[a]' -cores 4 -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k_u30.mp4"
 
-FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} \
--filter_complex 'multiscale_xma= outputs=5: \
-out_1_width=1280: out_1_height=720:  out_1_rate=full: \
-out_2_width=1280: out_2_height=720:  out_2_rate=half: \
-out_3_width=1920: out_3_height=1080: out_3_rate=half: \
-out_4_width=2560: out_4_height=1440: out_4_rate=half: \
-out_5_width=3840: out_5_height=2160: out_5_rate=half [a][b][c][d][e]; \
-[0:1]asplit=outputs=5[aud1][aud2][aud3][aud4][aud5]' \
--map '[a]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -map '[aud1]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p30fps.mp4 \
--map '[b]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -map '[aud2]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p15fps.mp4 \
--map '[c]' -cores 4 -b:v 4M -c:v mpsoc_vcu_h264 -map '[aud3]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1080p15fps.mp4 \
--map '[d]' -cores 4 -b:v 6M -c:v mpsoc_vcu_h264 -map '[aud4]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1440p15fps.mp4 \
--map '[e]' -cores 4 -b:v 8M -c:v mpsoc_vcu_h264 -map '[aud5]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_4k15fps.mp4"
-
+#FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} \
+#-filter_complex 'multiscale_xma= outputs=5: \
+#out_1_width=1280: out_1_height=720:  out_1_rate=full: \
+#out_2_width=1280: out_2_height=720:  out_2_rate=half: \
+#out_3_width=1920: out_3_height=1080: out_3_rate=half: \
+#out_4_width=2560: out_4_height=1440: out_4_rate=half: \
+#out_5_width=3840: out_5_height=2160: out_5_rate=half [a][b][c][d][e]; \
+#[0:1]asplit=outputs=5[aud1][aud2][aud3][aud4][aud5]' \
+#-map '[a]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -map '[aud1]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p30fps.mp4 \
+#-map '[b]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -map '[aud2]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p15fps.mp4 \
+#-map '[c]' -cores 4 -b:v 4M -c:v mpsoc_vcu_h264 -map '[aud3]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1080p15fps.mp4 \
+#-map '[d]' -cores 4 -b:v 6M -c:v mpsoc_vcu_h264 -map '[aud4]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1440p15fps.mp4 \
+#-map '[e]' -cores 4 -b:v 8M -c:v mpsoc_vcu_h264 -map '[aud5]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_4k15fps.mp4"
+FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} -filter_complex 'multiscale_xma=outputs=1:out_1_width=3840:out_1_height=2160:out_1_rate=full[a]' -map '[a]' -b:v 8M -c:v mpsoc_vcu_h264 -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_4k15fps.mp4"
 cmd="time ffmpeg -hide_banner ${FFMPEG_ARGS}"
 
 cmd_arr=(${cmd})
